2e91b9e51367481d81c17569afd14c134be838b7
Adesvian <adesvian2001@gmail.com>
2025-11-05 21:16:10 +0700

    fix: hide deprecation warning when compiling Sass with latest versions of Dart + Bootstrap

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
+            input: ["resources/sass/app.scss", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                silenceDeprecations: [
+                    "import",
+                    "color-functions",
+                    "global-builtin",
+                ],
+            },
+        },
+    },
 });
